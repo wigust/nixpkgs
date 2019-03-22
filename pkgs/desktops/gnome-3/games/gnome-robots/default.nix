bf87eb815deb30582bcbd10cef7315a9759e6797
worldofpeace <worldofpeace@users.noreply.github.com>
2019-03-22 01:21:51 -0400

    fixup! gnome3.gnome-robots: 3.22.3 -> 3.32.0

--- pkgs/desktops/gnome-3/games/gnome-robots/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-robots/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
 , librsvg, libcanberra-gtk3, gettext, itstool, libxml2, libgnome-games-support
-, libgee, meson, ninja, python3, desktop-file-utils }:
+, libgee, meson, ninja, python3, desktop-file-utils , hicolor-icon-theme, adwaita-icon-theme }:
 
 stdenv.mkDerivation rec {
   name = "gnome-robots-${version}";
@@ -17,11 +17,11 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     pkgconfig meson ninja python3
-    libxml2 gnome3.adwaita-icon-theme
-    wrapGAppsHook gettext itstool desktop-file-utils
+    libxml2 wrapGAppsHook gettext itstool desktop-file-utils
+    hicolor-icon-theme # For setup-hook
   ];
   buildInputs = [
-    gtk3 librsvg libcanberra-gtk3 libgnome-games-support libgee
+    gtk3 librsvg libcanberra-gtk3 libgnome-games-support libgee adwaita-icon-theme
   ];
 
   postPatch = ''
