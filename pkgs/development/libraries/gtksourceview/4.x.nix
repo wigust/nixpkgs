5ee5aea5130f6ac4f7d26a88dbdadb50a867eba0
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-22 17:04:16 +0100

    Revert "gtksourceview4: fixme: disable checks"
    This reverts commit 054e5feb1931f9b5b951c234ce54ba468816560b.

--- pkgs/development/libraries/gtksourceview/4.x.nix
+++ pkgs/development/libraries/gtksourceview/4.x.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  doCheck = false; # FIXME
+  doCheck = stdenv.isLinux;
   checkPhase = ''
     NO_AT_BRIDGE=1 \
     XDG_DATA_DIRS="$XDG_DATA_DIRS:${shared-mime-info}/share" \
