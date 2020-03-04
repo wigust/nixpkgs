215614edfc53f23912e55aff1286268d45ad314e
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-03-04 01:21:34 +0100

    libcroco: remove as it's no longer used
    librsvg and gnome-shell was the only derivations needing libcroco. Both
    of them have now dropped it as a dependency, so we can stop carrying it.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4555,8 +4555,6 @@ in
 
   libcoap = callPackage ../applications/networking/libcoap {};
 
-  libcroco = callPackage ../development/libraries/libcroco { };
-
   libcryptui = callPackage ../development/libraries/libcryptui { };
 
   libsmi = callPackage ../development/libraries/libsmi { };
